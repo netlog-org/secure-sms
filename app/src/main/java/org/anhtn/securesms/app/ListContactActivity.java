09d8f12dc2fbf30c6631135077bdce70809e99a1
Nhat Anh (ubuntu) <anhtn.bk@gmail.com>
2015-03-19 03:44:48 +0700

    Clean code

--- app/src/main/java/org/anhtn/securesms/app/ListContactActivity.java
+++ app/src/main/java/org/anhtn/securesms/app/ListContactActivity.java
@@ -2,10 +2,9 @@ package org.anhtn.securesms.app;
 
 import android.app.SearchManager;
 import android.content.Context;
-import android.content.Intent;
+import android.os.Bundle;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
