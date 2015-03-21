a096f3deab9acd5bc6bb2e5c339f5ee675c0d825
Nhat Anh (ubuntu) <anhtn.bk@gmail.com>
2015-03-22 03:38:45 +0700

    Clean code

--- app/src/main/java/org/anhtn/securesms/loaders/SmsContentLoader.java
+++ app/src/main/java/org/anhtn/securesms/loaders/SmsContentLoader.java
@@ -25,14 +25,14 @@ public class SmsContentLoader extends SimpleBaseLoader<List<SmsContentObject>> {
     @Override
     public List<SmsContentObject> loadInBackground() {
         Uri uri = Uri.parse("content://sms/");
-        String[] reqCols = new String[] {"_id", "body", "date", "type"};
+        String[] reqCols = new String[]{"_id", "body", "date", "type"};
         final List<SmsContentObject> results = new ArrayList<>();
 
         String address = mAddress;
         if (address == null) return null;
         String selection;
         try {
-            address = String.valueOf(Long.parseLong(address.toString()));
+            address = String.valueOf(Long.parseLong(address));
             selection = "address like '%" + address + "'";
         } catch (NumberFormatException ex) {
             selection = "address='" + address + "'";
@@ -40,30 +40,29 @@ public class SmsContentLoader extends SimpleBaseLoader<List<SmsContentObject>> {
 
         Cursor c = getContext().getContentResolver().query(uri, reqCols, selection,
                 null, "date ASC");
-        if (c.moveToFirst()) {
-            do {
-                SmsContentObject sms = new SmsContentObject();
-                sms.Type = c.getInt(c.getColumnIndex("type"));
-                sms.Id = c.getInt(c.getColumnIndex("_id"));
-                if (sms.Type != SmsContentObject.TYPE_INBOX
-                        && sms.Type != SmsContentObject.TYPE_SENT) {
+        while (c.moveToNext()) {
+            SmsContentObject sms = new SmsContentObject();
+            sms.Type = c.getInt(c.getColumnIndex("type"));
+            sms.Id = c.getInt(c.getColumnIndex("_id"));
+            if (sms.Type != SmsContentObject.TYPE_INBOX
+                    && sms.Type != SmsContentObject.TYPE_SENT) {
 
-                    Global.log("Ignore sms type: " + sms.Type);
-                    continue;
+                Global.log("Ignore sms type: " + sms.Type);
+                continue;
+            }
+            try {
+                Date date = new Date(Long.parseLong(c.getString(c.getColumnIndex("date"))));
+                sms.Date = DateFormat.getInstance().format(date);
+                String content = c.getString(c.getColumnIndex("body"));
+                if (content.startsWith(Global.MESSAGE_PREFIX)) {
+                    content = content.replace(Global.MESSAGE_PREFIX, "");
+                    content = AESHelper.decryptFromBase64(Global.DEFAULT_PASSWORD, content);
+                    if (content == null) continue;
                 }
-                try {
-                    Date date = new Date(Long.parseLong(c.getString(c.getColumnIndex("date"))));
-                    sms.Date = DateFormat.getInstance().format(date);
-                    String content = c.getString(c.getColumnIndex("body"));
-                    if (content.startsWith(Global.MESSAGE_PREFIX)) {
-                        content = content.replace(Global.MESSAGE_PREFIX, "");
-                        content = AESHelper.decryptFromBase64(Global.DEFAULT_PASSWORD, content);
-                        if (content == null) continue;
-                    }
-                    sms.Content = content;
-                    results.add(sms);
-                } catch (NumberFormatException ignored) { }
-            } while (c.moveToNext());
+                sms.Content = content;
+                results.add(sms);
+            } catch (NumberFormatException ignored) {
+            }
         }
         c.close();
 
