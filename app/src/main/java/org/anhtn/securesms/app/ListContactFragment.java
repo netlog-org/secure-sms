5e6c4a455ba1721644726c6e73fd0aad7c0e87d9
Nhat Anh (ubuntu) <anhtn.bk@gmail.com>
2015-03-22 04:42:26 +0700

    Clean code. Remove unused import

--- app/src/main/java/org/anhtn/securesms/app/ListContactFragment.java
+++ app/src/main/java/org/anhtn/securesms/app/ListContactFragment.java
@@ -5,7 +5,6 @@ import android.app.SearchManager;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.res.Resources;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Bundle;
