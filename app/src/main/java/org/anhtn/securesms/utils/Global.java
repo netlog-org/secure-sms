96a903014a4cbd767538b69039e5a8b0be0e7449
Nhat Anh (ubuntu) <anhtn.bk@gmail.com>
2015-03-25 01:56:33 +0700

    Change default passphrase and prefix of encrypted message

--- app/src/main/java/org/anhtn/securesms/utils/Global.java
+++ app/src/main/java/org/anhtn/securesms/utils/Global.java
@@ -6,8 +6,8 @@ import java.util.List;
 
 public class Global {
 
-    public static final String DEFAULT_PASSPHRASE = "securesms";
-    public static final String MESSAGE_PREFIX = "AES-128 ";
+    public static final String DEFAULT_PASSPHRASE = "%1q2W3e4R5ta@_[}&#";
+    public static final String MESSAGE_PREFIX = "$]";
     public static final String APP_PASSPHRASE_KEY = "app.passphrase";
 
     public static boolean smartContains(String s1, String s2, List<Integer> matchedPos) {
